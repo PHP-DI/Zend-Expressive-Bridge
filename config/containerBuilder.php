3706c4d9798ef8dc70fad3779dacfe99514c33c6
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2017-01-21 20:18:06 +0100

    Do not rely on one specific routing / templating approach

--- config/containerBuilder.php
+++ config/containerBuilder.php
@@ -3,5 +3,7 @@ declare(strict_types=1);
 
 return (new \DI\ContainerBuilder())
     ->addDefinitions(__DIR__ . '/autoload/dependencies.php')
+    ->addDefinitions(__DIR__ . '/autoload/dependencies.router.php')
+    ->addDefinitions(__DIR__ . '/autoload/dependencies.templating.php')
     ->addDefinitions(__DIR__ . '/autoload/errorHandler.php')
     ->addDefinitions(__DIR__ . '/autoload/middleware-pipeline.php');
